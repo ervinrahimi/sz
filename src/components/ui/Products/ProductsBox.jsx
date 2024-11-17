43824759f3c61e3f7242a6d21438cad3e609215b
ervin rahimi <ervinrhimi@gmail.com>
2024-11-17 21:08:34 +0330

    update card box

--- src/components/ui/Products/ProductsBox.jsx
+++ src/components/ui/Products/ProductsBox.jsx
@@ -7,6 +7,17 @@ import styles from './ProductsBox.module.css'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import toast from 'react-hot-toast'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+import './ProductBoxStyles.css'
+
+// import required modules
+import { Pagination } from 'swiper/modules'
 
 export default function ProductsBox({ title, subTitle, cardBoxes }) {
   const router = useRouter()
@@ -34,44 +45,106 @@ export default function ProductsBox({ title, subTitle, cardBoxes }) {
         <p>{subTitle}</p>
       </div>
       <div className={styles.container}>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          centeredSlides={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {cardBoxes &&
+            cardBoxes.map((box) => (
+              <>
+                <SwiperSlide>
+                  {' '}
+                  <div key={box.id} className={styles.box}>
+                    <div className={styles.colorContainer}>
+                      <span className={styles.colors} />
+                      <span className={styles.colors} />
+                      <span className={styles.colors} />
+                    </div>
+                    <Image
+                      className={styles.image}
+                      src={box.car.image[0]} // لینک عکس خودرو
+                      width={1000}
+                      height={1000}
+                      alt={'car-image'}
+                      onClick={handleViewClick(box.viewLink)}
+                    />
+                    <h4 className={styles.title} onClick={handleViewClick(box.viewLink)}>
+                      {box.car.name}
+                    </h4>
+                    <h3 className={styles.subTitle} onClick={handleViewClick(box.viewLink)}>
+                      {box.subtitle}
+                    </h3>
+                    <p className={styles.text}>{box.description.slice(0, 98) + '...'}</p>
+                    <div className={styles.line}>
+                      <span></span>
+                    </div>
+                    <div className={styles.actionBox}>
+                      <button
+                        className={styles.actionButton}
+                        onClick={handleViewClick(box.viewLink)}
+                      >
+                        شرایط فروش
+                      </button>
+                      <button
+                        className={styles.actionButton}
+                        onClick={handleCatalogDownload(box.catalogUrl)}
+                      >
+                        دانلود کاتالوگ
+                      </button>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              </>
+            ))}
+        </Swiper>
+      </div>
+      <div className={styles.container}>
         {cardBoxes &&
           cardBoxes.map((box) => (
-            <div key={box.id} className={styles.box}>
-              <div className={styles.colorContainer}>
-                <span className={styles.colors} />
-                <span className={styles.colors} />
-                <span className={styles.colors} />
+            <>
+              <div key={box.id} className={styles.box}>
+                <div className={styles.colorContainer}>
+                  <span className={styles.colors} />
+                  <span className={styles.colors} />
+                  <span className={styles.colors} />
+                </div>
+                <Image
+                  className={styles.image}
+                  src={box.car.image[0]} // لینک عکس خودرو
+                  width={1000}
+                  height={1000}
+                  alt={'car-image'}
+                  onClick={handleViewClick(box.viewLink)}
+                />
+                <h4 className={styles.title} onClick={handleViewClick(box.viewLink)}>
+                  {box.car.name}
+                </h4>
+                <h3 className={styles.subTitle} onClick={handleViewClick(box.viewLink)}>
+                  {box.subtitle}
+                </h3>
+                <p className={styles.text}>{box.description.slice(0, 98) + '...'}</p>
+                <div className={styles.line}>
+                  <span></span>
+                </div>
+                <div className={styles.actionBox}>
+                  <button className={styles.actionButton} onClick={handleViewClick(box.viewLink)}>
+                    شرایط فروش
+                  </button>
+                  <button
+                    className={styles.actionButton}
+                    onClick={handleCatalogDownload(box.catalogUrl)}
+                  >
+                    دانلود کاتالوگ
+                  </button>
+                </div>
               </div>
-              <Image
-                className={styles.image}
-                src={box.car.image[0]} // لینک عکس خودرو
-                width={1000}
-                height={1000}
-                alt={'car-image'}
-                onClick={handleViewClick(box.viewLink)}
-              />
-              <h4 className={styles.title} onClick={handleViewClick(box.viewLink)}>
-                {box.car.name}
-              </h4>
-              <h3 className={styles.subTitle} onClick={handleViewClick(box.viewLink)}>
-                {box.subtitle}
-              </h3>
-              <p className={styles.text}>{box.description.slice(0, 98) + '...'}</p>
-              <div className={styles.line}>
-                <span></span>
-              </div>
-              <div className={styles.actionBox}>
-                <button className={styles.actionButton} onClick={handleViewClick(box.viewLink)}>
-                  شرایط فروش
-                </button>
-                <button
-                  className={styles.actionButton}
-                  onClick={handleCatalogDownload(box.catalogUrl)}
-                >
-                  دانلود کاتالوگ
-                </button>
-              </div>
-            </div>
+            </>
           ))}
       </div>
     </>
